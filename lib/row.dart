9ee8dba6ed12916a128d20852a0c73721275de94
mohamed <dr.m.tawab2@gmail.com>
2022-08-02 23:19:08 +0200

    add image and 4 zekrs

---
+++ lib/row.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyZekr extends StatelessWidget {
+  MyZekr(
+      {super.key,
+      required this.onPressed,
+      this.data,
+      required this.title,
+      // required this.image
+      });
+  int? data;
+  final String title;
+  // add image
+  // final String image;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(8),
+        height: MediaQuery.of(context).size.height * 0.12,
+        child: Card(
+          color: Colors.amber[300],
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            
+            Container(
+              width: 100,
+              child: Text(
+                title,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: onPressed,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.amber[500],
+              ),
+              child: const Icon(
+                Icons.arrow_circle_up,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              data.toString(),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ]),
+        ));
+  }
+}
